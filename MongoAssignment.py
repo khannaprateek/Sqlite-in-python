d3a35bd70d2256491adaaa043b230a790ad40832
khannaprateek <37430092+khannaprateek@users.noreply.github.com>
2018-09-08 11:05:31 +0530

    Update MongoAssignment.py

--- MongoAssignment.py
+++ MongoAssignment.py
@@ -14,7 +14,6 @@ for i in range(1,11):
             else:
                 print("Please enter your marks between the range(0-100)")
         l.append(d)
-        print(l)
 collection.insert_many(l)
 data = collection.find({'Marks':{'$gt':80}})
 for d in data:
